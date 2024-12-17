eabb5151809b304a1fb9a22f182e0a7cd80f6e32
TrinhDinhHoang29 <162282399+TrinhDinhHoang29@users.noreply.github.com>
2024-12-17 20:30:27 +0700

    Setup CORS

--- PrBeleBackend.API/Program.cs
+++ PrBeleBackend.API/Program.cs
@@ -1,4 +1,4 @@
-using PrBeleBackend.API.StartupExtensions;
+﻿using PrBeleBackend.API.StartupExtensions;
 using PrBeleBackend.Infrastructure.Seeder;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -6,8 +6,18 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 
 builder.Services.ServiceConfigure(builder.Configuration);
-
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("Allow3000", policy =>
+    {
+        policy.WithOrigins("http://localhost:3000") // Cho phép từ cổng 3000
+              .AllowAnyHeader() // Cho phép mọi Header
+              .AllowAnyMethod(); // Cho phép mọi phương thức (GET, POST, PUT, DELETE, ...)
+    });
+});
 var app = builder.Build();
+
+app.UseCors("Allow3000");
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
